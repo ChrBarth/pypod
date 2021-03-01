06a3b7d3962c281be080e88fc57f59c8c0048f1b
ChrBarth <ChrBarth@users.noreply.github.com>
2021-03-01 16:11:26 +0100

    added line6.py, safer opening in- and output
    moved the whole PROGRAMS, amp_names etc. to line6.py
    added try and except to open_input and open_output
    (open_input("") gives an error, open_input() works, in case
    there is no USB MIDI device found)

---
+++ line6.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+
+# Some useful POD-Variables
+
+# The Program names:
+PROGRAMS = [ "1A", "1B", "1C", "1D",
+             "2A", "2B", "2C", "2D",
+             "3A", "3B", "3C", "3D",
+             "4A", "4B", "4C", "4D",
+             "5A", "5B", "5C", "5D",
+             "6A", "6B", "6C", "6D",
+             "7A", "7B", "7C", "7D",
+             "8A", "8B", "8C", "8D",
+             "9A", "9B", "9C", "9D" ]
+
+# The Amp Models:
+amp_names = [
+    'Tube Preamp',
+    'POD Clean Line 6',
+    'POD Crunch Line 6',
+    'POD Drive Line 6',
+    'POD Layer Line 6',
+    'Small Tweed',
+    'Tweed Blues',
+    'Black Panel',
+    'Modern Class A',
+    'Brit Class A',
+    'Brit Blues',
+    'Brit Classic',
+    'Brit Hi Gain',
+    'Rectified ’94',
+    'Modern Hi Gain',
+    'Fuzz Box',
+    'Jazz Clean',
+    'Boutique #1',
+    'Boutique #2',
+    'Brit Class A #2',
+    'Brit Class A #3',
+    'Small Tweed #2',
+    'Black Panel #2',
+    'Boutique #3',
+    'California Crunch #1',
+    'California Crunch #2',
+    'Rectified #2',
+    'Modern Hi Gain #2',
+    'Line 6 Twang',
+    'Line 6 Crunch #2',
+    'Line 6 Blues',
+    'Line 6 Insane' ]
+
+# The Cab names:
+cab_names = [
+    "1x 8 ’60 Fender Tweed Champ",
+    "1x12 ’52 Fender Tweed Deluxe",
+    "1x12 ’60 Vox AC15",
+    "1x12 ’64 Fender Blackface Deluxe",
+    "1x12 ’98 Line 6 Flextone",
+    "2x12 ’65 Fender Blackface Twin",
+    "2x12 ’67 VOX AC30",
+    "2x12 ’95 Matchless Chieftain",
+    "2x12 ’98 Pod custom 2x12",
+    "4x10 ’59 Fender Bassman",
+    "4x10 ’98 Pod custom 4x10 cab",
+    "4x12 ’96 Marshall with V30s",
+    "4x12 ’78 Marshall with 70s",
+    "4x12 ’97 Marshall off axis",
+    "4x12 ’98 Pod custom 4x12",
+    "No Cabinet" ]
+
+# The effect types:
+fx_names = [
+    "Chorus2",
+    "Flanger1",
+    "Rotary",
+    "Flanger2",
+    "Delay/Chorus1",
+    "Delay/Tremolo",
+    "Delay",
+    "Delay/Comp",
+    "Chorus1",
+    "Tremolo",
+    "Bypass",
+    "Compressor",
+    "Delay/Chorus2",
+    "Delay/Flanger1",
+    "Delay/Swell",
+    "Delay/Flanger2" ]
--- pypod.py
+++ pypod.py
@@ -4,6 +4,7 @@ import subprocess
 #import threading
 import time
 import mido
+import line6
 
 # {{{ MIDI init:
 MIDI_IN  = ""
@@ -24,97 +25,10 @@ program_name = ""
 
 # }}}
 
-# {{{ Some useful POD-Variables
-
-# The Program names:
-PROGRAMS = [ "1A", "1B", "1C", "1D",
-             "2A", "2B", "2C", "2D",
-             "3A", "3B", "3C", "3D",
-             "4A", "4B", "4C", "4D",
-             "5A", "5B", "5C", "5D",
-             "6A", "6B", "6C", "6D",
-             "7A", "7B", "7C", "7D",
-             "8A", "8B", "8C", "8D",
-             "9A", "9B", "9C", "9D" ]
-
-# The Amp Models:
-amp_names = [
-    'Tube Preamp',
-    'POD Clean Line 6',
-    'POD Crunch Line 6',
-    'POD Drive Line 6',
-    'POD Layer Line 6',
-    'Small Tweed',
-    'Tweed Blues',
-    'Black Panel',
-    'Modern Class A',
-    'Brit Class A',
-    'Brit Blues',
-    'Brit Classic',
-    'Brit Hi Gain',
-    'Rectified ’94',
-    'Modern Hi Gain',
-    'Fuzz Box',
-    'Jazz Clean',
-    'Boutique #1',
-    'Boutique #2',
-    'Brit Class A #2',
-    'Brit Class A #3',
-    'Small Tweed #2',
-    'Black Panel #2',
-    'Boutique #3',
-    'California Crunch #1',
-    'California Crunch #2',
-    'Rectified #2',
-    'Modern Hi Gain #2',
-    'Line 6 Twang',
-    'Line 6 Crunch #2',
-    'Line 6 Blues',
-    'Line 6 Insane' ]
-
-# The Cab names:
-cab_names = [
-    "1x 8 ’60 Fender Tweed Champ",
-    "1x12 ’52 Fender Tweed Deluxe",
-    "1x12 ’60 Vox AC15",
-    "1x12 ’64 Fender Blackface Deluxe",
-    "1x12 ’98 Line 6 Flextone",
-    "2x12 ’65 Fender Blackface Twin",
-    "2x12 ’67 VOX AC30",
-    "2x12 ’95 Matchless Chieftain",
-    "2x12 ’98 Pod custom 2x12",
-    "4x10 ’59 Fender Bassman",
-    "4x10 ’98 Pod custom 4x10 cab",
-    "4x12 ’96 Marshall with V30s",
-    "4x12 ’78 Marshall with 70s",
-    "4x12 ’97 Marshall off axis",
-    "4x12 ’98 Pod custom 4x12",
-    "No Cabinet" ]
-
-# The effect types:
-fx_names = [
-    "Chorus2",
-    "Flanger1",
-    "Rotary",
-    "Flanger2",
-    "Delay/Chorus1",
-    "Delay/Tremolo",
-    "Delay",
-    "Delay/Comp",
-    "Chorus1",
-    "Tremolo",
-    "Bypass",
-    "Compressor",
-    "Delay/Chorus2",
-    "Delay/Flanger1",
-    "Delay/Swell",
-    "Delay/Flanger2" ]
 
 CH = 1 # the default pod2 midi-channel
 # pod-midi-commands (CC)
 AMP_MODEL = 12 # get/set amp-model
-
-# }}}
 
 # {{{ general functions:
 def hextoint(hexstring):
@@ -142,7 +56,7 @@ def nib(in_byte):
 
 def dump_program(program, midi_port):
     # dump a single program from the pod:
-    msg = mido.Message('sysex', data=[0x00, 0x01, 0x0c, 0x01, 0x00, 0x00, PROGRAMS.index(program)])
+    msg = mido.Message('sysex', data=[0x00, 0x01, 0x0c, 0x01, 0x00, 0x00, line6.PROGRAMS.index(program)])
     midi_port.send(msg)
 
 # }}}
@@ -167,9 +81,17 @@ def monitor_input(message):
         #print(msg_bytes[:])
         #print(f"Program name: {program_name}")
 
-inport = mido.open_input(MIDI_IN)
+try:
+    inport = mido.open_input(MIDI_IN)
+except OSError:
+    # open default instead:
+    inport = mido.open_input()
 inport.callback = monitor_input
-outport = mido.open_output(MIDI_OUT)
+
+try:
+    outport = mido.open_output(MIDI_OUT)
+except OSError:
+    outport = mido.open_output()
 
 dump_program("1A", outport)
 time.sleep(1) # we need some time so the callback-function can grab the response
